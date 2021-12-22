88717726ce0c6dcc2bfbcf671c8c2f6ed13986d1
Jámbor Alexandra <ja20000907@gmail.com>
2021-12-22 21:21:16 +0100

    Implemented DB class

--- php/core/DB.php
+++ php/core/DB.php
@@ -1,31 +1,40 @@
 <?php
 
+DB::init();
+
 class DB
 {
-    private PDO $pdo;
+    private static PDO $pdo;
 
-    public function __construct()
+    public static function init(): void
     {
-        $this->pdo = new PDO(
-            'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
-            DB_USER,
-            DB_PASSWORD,
-            [
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES   => false,
-            ]
-        );
+        try {
+            self::$pdo = new PDO(
+                'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+                DB_USER,
+                DB_PASSWORD,
+                [
+                    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES   => false,
+                ]
+            );
+            self::$pdo->query('SELECT 1')->fetchAll(); // SQL self test
+        } catch (PDOException) {
+            Response::serverError('MySQL server is down');
+        }
     }
 
-    public function query(string $sql, array $parameters = []) {
-        $stmt = $this->pdo->prepare($sql);
+    public static function query(string $sql, array $parameters = []): bool|array
+    {
+        $stmt = self::$pdo->prepare($sql);
         $stmt->execute($parameters);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function execute(string $sql, array $parameters = []) {
-        $stmt = $this->pdo->prepare($sql);
+    public static function execute(string $sql, array $parameters = []): int
+    {
+        $stmt = self::$pdo->prepare($sql);
         $stmt->execute($parameters);
         return $stmt->rowCount();
     }
--- php/core/config.php
+++ php/core/config.php
@@ -1,6 +1,5 @@
 <?php
 const DB_HOST = 'localhost';
 const DB_NAME = 'rest';
-const DB_PORT = 3306;
 const DB_USER = 'root';
 const DB_PASSWORD = '';
