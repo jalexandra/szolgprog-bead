f2701b9967c6a18b4225b0465abc5f985422229b
Jámbor Alexandra <ja20000907@gmail.com>
2021-12-22 21:26:53 +0100

    Implemented Router

--- php/core/router.php
+++ php/core/router.php
@@ -1,8 +1,17 @@
 <?php
+if(!isset($_GET['m'])){
+    $_GET['m'] = '';
+}
+if(!isset($_GET['v'])){
+    $_GET['v'] = 'get';
+}
+if (!isset($_GET['id'])){
+    $_GET['id'] = '';
+}
 switch ($_GET['m']) {
     case 'user':
-        require_once '../controllers/user.controller.php';
+        require_once 'controllers/user.controller.php';
         break;
     default:
-        require_once '../controllers/not-found.controller.php';
+        require_once 'controllers/not-found.controller.php';
 }
--- php/index.php
+++ php/index.php
@@ -1,6 +1,9 @@
 <?php
 
+require_once 'utils/response.php';
 require_once 'core/config.php';
 require_once 'core/DB.php';
+require_once 'utils/Validation.php';
 require_once 'utils/error.php';
+require_once 'core/guards.php';
 require_once 'core/router.php';
